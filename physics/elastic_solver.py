finally working but wrong result

physics/elastic_solver.py
@@ -1,6 +1,7 @@
 import ufl
+import basix.ufl
 from dolfinx import fem
-from dolfinx.fem.petsc import assemble_matrix # Прямой импорт!
+from dolfinx.fem.petsc import assemble_matrix
 from petsc4py import PETSc
 from slepc4py import SLEPc
 import numpy as np
@@ -15,32 +16,38 @@ class ElasticSolver:
     def __init__(self, domain, cell_tags, mat_dict, q_b):
         self.domain = domain
         self.cell_tags = cell_tags
+        self.mat_dict = mat_dict
         self.q_b = q_b
         
-        # Надежный способ создания векторного пространства для новых версий
-        v_el = ufl.VectorElement("Lagrange", domain.ufl_cell(), 2)
+        v_el = basix.ufl.element("Lagrange", domain.topology.cell_name(), 2, shape=(3,))
         self.V = fem.functionspace(domain, v_el)
         
-        self.rho = fem.Function(fem.functionspace(domain, ("DG", 0)))
-        
+        V_rho = fem.functionspace(domain, ("DG", 0))
+        self.rho = fem.Function(V_rho)
         for tag, mat in mat_dict.items():
             cells = cell_tags.find(tag)
-            self.rho.x.array[cells] = mat.rho
+            if len(cells) > 0:
+                self.rho.x.array[cells] = mat.rho
             
     def solve(self, guess_freq, n_modes=5):
-        u = ufl.TrialFunction(self.V)
-        v = ufl.TestFunction(self.V)
+        u, v = ufl.TrialFunction(self.V), ufl.TestFunction(self.V)
         dx = ufl.Measure("dx", domain=self.domain, subdomain_data=self.cell_tags)
-        
-        C_dummy = 1.0 
         
         grad_u = eps_T(u) + 1j * self.q_b * eps_Z(u)
         grad_v = eps_T(v) + 1j * self.q_b * eps_Z(v)
         
-        a = ufl.inner(C_dummy * grad_u, grad_v) * dx
+        a = None
+        for tag, mat in self.mat_dict.items():
+            C_np = mat.get_stiffness_tensor()
+            if mat.name == "Air": C_np = np.eye(6) * 1e-6
+            
+            C_mat = ufl.as_matrix(C_np.tolist())
+            stress_u = ufl.dot(C_mat, grad_u)
+            term = ufl.inner(stress_u, grad_v) * dx(tag)
+            a = term if a is None else a + term
+            
         b = self.rho * ufl.inner(u, v) * dx
         
-        # Используем assemble_matrix напрямую
         A = assemble_matrix(fem.form(a))
         A.assemble()
         B = assemble_matrix(fem.form(b))
@@ -59,18 +66,16 @@ class ElasticSolver:
         st.setType(SLEPc.ST.Type.SINVERT)
         eigensolver.solve()
         
-        print(f"Найдено акустических мод: {eigensolver.getConverged()}")
         if eigensolver.getConverged() > 0:
             val = eigensolver.getEigenvalue(0)
-            freq = np.sqrt(val.real) / (2 * np.pi)
-            
-            # Достаем собственную функцию
+            freq = np.sqrt(max(0, val.real)) / (2 * np.pi)
             vr, vi = A.getVecs()
             eigensolver.getEigenvector(0, vr, vi)
             u_func = fem.Function(self.V)
             u_func.x.array[:] = vr.array + 1j * vi.array
-            
             return u_func, freq
         else:
             raise RuntimeError("Упругий решатель не сошелся!")
+
+
 

physics/em_solver.py
@@ -1,12 +1,13 @@
 import ufl
+import basix.ufl
 from dolfinx import fem
+from dolfinx import mesh as dmesh
 from dolfinx.fem.petsc import assemble_matrix
 from petsc4py import PETSc
 from slepc4py import SLEPc
 import numpy as np
 
 def curl_t(w):
-    """Поперечный ротор 2D вектора"""
     return w[1].dx(0) - w[0].dx(1)
 
 class EMSolver:
@@ -16,62 +17,76 @@ class EMSolver:
         self.wavelength = wavelength
         self.k0_sq = (2.0 * np.pi / wavelength)**2
         
-        # Смешанное пространство: Неделек (поперечное поле Et) + Лагранж (продольное Ez)
-        # В FEniCSx смешанные элементы задаются так:
-        N = ufl.FiniteElement("Nedelec 1st kind H(curl)", domain.ufl_cell(), 1)
-        L = ufl.FiniteElement("Lagrange", domain.ufl_cell(), 1)
-        mix_el = ufl.MixedElement([N, L])
+        # Правильное создание смешанного пространства элементов
+        cell_name = domain.topology.cell_name()
+        N = basix.ufl.element("N1curl", cell_name, 1)
+        L = basix.ufl.element("Lagrange", cell_name, 1)
+        mix_el = basix.ufl.mixed_element([N, L])
         self.V = fem.functionspace(domain, mix_el)
         
-        # Задаем диэлектрическую проницаемость eps_r (кусочно-постоянную)
         V_eps = fem.functionspace(domain, ("DG", 0))
         self.eps_r = fem.Function(V_eps)
         for tag, mat in mat_dict.items():
             cells = cell_tags.find(tag)
-            self.eps_r.x.array[cells] = mat.n**2  # eps = n^2
+            if len(cells) > 0:
+                self.eps_r.x.array[cells] = mat.n**2
 
     def solve(self, guess_neff, n_modes=2):
-        # Пробные и тестовые функции
-        u = ufl.TrialFunction(self.V)
-        v = ufl.TestFunction(self.V)
-        
-        # Разделяем на поперечную (t) и продольную (z) части
+        u, v = ufl.TrialFunction(self.V), ufl.TestFunction(self.V)
         Et, Ez = ufl.split(u)
         Vt, Vz = ufl.split(v)
         
         dx = ufl.Measure("dx", domain=self.domain, subdomain_data=self.cell_tags)
-        
-        # Слабая форма электромагнитного уравнения (из диссертации Elan Lezar)
-        # Обрати внимание: в FEniCSx мы пишем математику напрямую!
-        u_r = 1.0 # магнитная проницаемость
+        u_r = 1.0 
         
         s_tt = (1.0/u_r) * ufl.inner(curl_t(Et), curl_t(Vt))
         t_tt = self.eps_r * ufl.inner(Et, Vt)
         s_zz = (1.0/u_r) * ufl.inner(ufl.grad(Ez), ufl.grad(Vz))
-        t_zz = self.eps_r * Ez * Vz
+        t_zz = self.eps_r * ufl.inner(Ez, Vz) 
         
         b_tt = (1.0/u_r) * ufl.inner(Et, Vt)
         b_tz = (1.0/u_r) * ufl.inner(Et, ufl.grad(Vz))
         b_zt = (1.0/u_r) * ufl.inner(ufl.grad(Ez), Vt)
         
-        # Уравнения для левой (A) и правой (B) матриц
         a = (s_tt - self.k0_sq * t_tt) * dx
         b = (s_zz - self.k0_sq * t_zz + b_tt + b_tz + b_zt) * dx
         
-        # Сборка матриц PETSc
-        A = assemble_matrix(fem.form(a))
-        A.assemble()
-        B = assemble_matrix(fem.form(b))
-        B.assemble()
+        # Граничные условия
+        self.domain.topology.create_connectivity(self.domain.topology.dim - 1, self.domain.topology.dim)
+        boundary_facets = dmesh.exterior_facet_indices(self.domain.topology)
         
-        # Настройка SLEPc решателя (поиск собственных значений)
+        bcs = []
+        if len(boundary_facets) > 0:
+            for i in range(2): # Для Et и для Ez
+                V_sub, _ = self.V.sub(i).collapse()
+                dofs = fem.locate_dofs_topological((self.V.sub(i), V_sub), self.domain.topology.dim - 1, boundary_facets)
+                val = fem.Function(V_sub)
+                val.x.array[:] = 0.0
+                bcs.append(fem.dirichletbc(val, dofs, self.V.sub(i)))
+        
+        A = assemble_matrix(fem.form(a), bcs=bcs)
+        A.assemble()
+        
+        B = assemble_matrix(fem.form(b))
+        B.assemble() # СНАЧАЛА СОБИРАЕМ
+        
+        bc_indices = []
+        for bc in bcs:
+            dofs = bc.dof_indices()
+            if isinstance(dofs, tuple): dofs = dofs[0]
+            bc_indices.append(np.asarray(dofs, dtype=np.int32))
+        
+        if len(bc_indices) > 0:
+            flattened_dofs = np.unique(np.concatenate(bc_indices)).astype(np.int32)
+            B.zeroRowsLocal(flattened_dofs, diag=0.0) # ТЕПЕРЬ ОБНУЛЯЕМ
+            B.assemble() # И ЕЩЕ РАЗ ФИКСИРУЕМ
+        
         eigensolver = SLEPc.EPS().create()
         eigensolver.setOperators(A, B)
         eigensolver.setProblemType(SLEPc.EPS.ProblemType.GNHEP)
         eigensolver.setDimensions(nev=n_modes)
         
-        # Ищем моду вокруг эффективного показателя преломления (guess_neff)
-        shift = -(2.0 * np.pi * guess_neff / self.wavelength)**2
+        shift = (2.0 * np.pi * guess_neff / self.wavelength)**2
         eigensolver.setTarget(shift)
         eigensolver.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_MAGNITUDE)
         
@@ -79,18 +94,15 @@ class EMSolver:
         st.setType(SLEPc.ST.Type.SINVERT)
         eigensolver.solve()
         
-        # Возвращаем оптическое поле E и эффективный индекс
         if eigensolver.getConverged() > 0:
             val = eigensolver.getEigenvalue(0)
-            neff = np.sqrt(-val.real) * self.wavelength / (2 * np.pi)
-            
-            # Извлекаем вектор поля
+            neff = np.sqrt(val.real) * self.wavelength / (2 * np.pi)
             vr, vi = A.getVecs()
             eigensolver.getEigenvector(0, vr, vi)
-            
             E_func = fem.Function(self.V)
             E_func.x.array[:] = vr.array + 1j * vi.array
             return E_func, neff
         else:
             raise RuntimeError("ЭМ-решатель не сошелся!")
+
 

physics/gain_calc.py
@@ -7,56 +7,37 @@ class BrillouinGainCalculator:
     def __init__(self, domain, cell_tags, facet_tags):
         self.domain = domain
         self.cell_tags = cell_tags
-        self.facet_tags = facet_tags # Понадобится для интегралов по границам (dS)
+        self.facet_tags = facet_tags
         self.dx = ufl.Measure("dx", domain=domain, subdomain_data=cell_tags)
         self.dS = ufl.Measure("dS", domain=domain, subdomain_data=facet_tags)
 
     def calculate_total_gain(self, E_opt, u_mech, omega_opt, omega_mech, Q_mech, power_opt, P_mech, mat_core, mat_clad):
-        """
-        Полный расчет усиления ВРМБ, объединяющий объемную электрострикцию
-        и радиационное давление на границах.
-        """
-        # 1. Извлекаем компоненты полей
         Et, Ez = ufl.split(E_opt)
         E_vec = ufl.as_vector([Et[0], Et[1], Ez])
+        E_sq = ufl.inner(E_vec, E_vec)
         
-        # 2. Объемная электрострикция (Bulk Electrostriction)
-        # В UFL мы можем написать тензор прямо как математическую матрицу:
-        p11, p12 = mat_core.p11, mat_core.p12
+        p12 = mat_core.p12
         eps_core = mat_core.n**2
         
-        # Для простоты показываем скалярный пример сжатия поля.
-        # В полной версии здесь перемножение тензора p_ijkl на E_k * E_l^*
-        E_sq = ufl.inner(E_vec, E_vec)
-        # Упрощенная сила электрострикции (градиент плотности энергии):
-        f_bulk = -0.5 * const.epsilon_0 * (eps_core**2) * p12 * ufl.grad(E_sq)
-        
-        # Интеграл перекрытия объема: \int (f_bulk * u_mech^*) dx
+        # Объемная сила
+        grad_E_sq = ufl.grad(E_sq)
+        f_bulk = -0.5 * const.epsilon_0 * (eps_core**2) * p12 * ufl.as_vector([grad_E_sq[0], grad_E_sq[1], 0.0])
         overlap_bulk = fem.assemble_scalar(fem.form(ufl.inner(f_bulk, u_mech) * self.dx(2)))
         
-        # 3. Радиационное давление на границе (Radiation Pressure)
-        # Прыжок тензора напряжений Максвелла на интерфейсе ядро(2)-оболочка(1)
-        # n_vec - вектор нормали к границе
-        n_vec = ufl.FacetNormal(self.domain)
+        # Поверхностная сила (давление)
+        n_2d = ufl.FacetNormal(self.domain)
+        n_vec = ufl.as_vector([n_2d[0], n_2d[1], 0.0])
         
-        # Вычисляем тензор напряжений T_ij для кремния (+) и воздуха (-)
-        T_core = 0.5 * const.epsilon_0 * eps_core * (ufl.outer(E_vec, E_vec) - 0.5 * E_sq * ufl.Identity(3))
-        T_clad = 0.5 * const.epsilon_0 * (mat_clad.n**2) * (ufl.outer(E_vec, E_vec) - 0.5 * E_sq * ufl.Identity(3))
+        V_eps = fem.functionspace(self.domain, ("DG", 0))
+        eps_f = fem.Function(V_eps)
+        eps_f.x.array[self.cell_tags.find(1)] = mat_clad.n**2
+        eps_f.x.array[self.cell_tags.find(2)] = mat_core.n**2
         
-        # Прыжок напряжений на внутренней границе (dS)
-        f_boundary = ufl.jump(T_core, n_vec) - ufl.jump(T_clad, n_vec)
+        T = 0.5 * const.epsilon_0 * eps_f * (ufl.outer(E_vec, ufl.conj(E_vec)) - 0.5 * E_sq * ufl.Identity(3))
+        overlap_bdr = fem.assemble_scalar(fem.form(ufl.inner(ufl.jump(T, n_vec), u_mech('-')) * self.dS(10)))
         
-        # Интеграл перекрытия поверхности (Boundary coupling): \int (f_boundary * u_mech^*) dS
-        # В реальной задаче граница (интерфейс) должна быть помечена тегом в facet_tags (например, тег 10)
-        overlap_bdr = fem.assemble_scalar(fem.form(ufl.inner(f_boundary, u_mech('-')) * self.dS(10)))
-        
-        # 4. Итоговое усиление (формула 14 из статьи)
         total_overlap = overlap_bulk + overlap_bdr
-        
-        # Префактор (учитывающий качество резонанса Q_mech и мощности)
         prefactor = (Q_mech * omega_opt) / (4.0 * (power_opt**2) * P_mech) * 1e21
-        
-        gain = prefactor * np.abs(total_overlap)**2
-        return gain
+        return prefactor * np.abs(total_overlap)**2
 
 